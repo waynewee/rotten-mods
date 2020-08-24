c489cdc445ae0c09358124ba2d236ffab563e1da
Chester Sim <chestersimdy@gmail.com>
2020-08-24 16:12:08 +0800

    Set up NextJS and airbnb-eslint for frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>RottenMods</title>
+      </Head>
+      <div>
+        <h1>Welcome to RottenMods</h1>
+      </div>
+    </>
+  )
+}
+
+export default Index;
