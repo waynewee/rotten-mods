29a3f7e77a903b2dd2ee368de6023bba91593977
waynewee <wwayne.wee@gmail.com>
2020-10-22 15:11:03 +0800

    added routes for all school & course

--- api/src/routes/school.ts
+++ api/src/routes/school.ts
@@ -5,6 +5,7 @@ const router = express.Router()
 
 import * as schoolController from '../controllers/schoolController' 
 
+router.get("/", schoolController.allSchools)
 router.get("/:id", schoolController.getSchool)
 router.put("/", schoolController.addSchool)
 router.delete("/:id", schoolController.deleteSchool)
