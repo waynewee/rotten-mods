fbd66dfd7356ceb1246bb0d5c72a2ca333053a7d
waynewee <wwayne.wee@gmail.com>
2020-10-22 14:38:45 +0800

    changed email validation

--- api/src/helpers/validators.ts
+++ api/src/helpers/validators.ts
@@ -4,6 +4,5 @@ export function validateString(str: string): boolean{
 
 export function validateEmail(email: string): boolean {
   const arr = email.split("@")[1].split(".")
-  const arr_ = arr.slice(arr.length - 2, arr.length)
-  return arr_[0] == "edu" && arr_[1] == "sg"
+  return arr[arr.length - 1] == "edu"
 }
