ceb3230c0369a67dae9b565736b91974e20e9a2e
rongrongrr <jingrong.contact@gmail.com>
2020-10-28 04:25:06 +0800

    Fix Bugs

--- api/src/utils/recommend/recommendByLikes.js
+++ api/src/utils/recommend/recommendByLikes.js
@@ -10,108 +10,129 @@ const Mod = require('../../../dist/src/models/Mod').default
 const Recommendation = require('../../../dist/src/models/Recommendation').default
 const { recommendationTypesEnum } = require('../../../dist/src/models/Recommendation')
 var _ = require('lodash');
+var ObjectId = require('mongodb').ObjectId;
+
+/*
+to generate recommendation
+- sum(jaccard of user and all users who liked mod) / total likes for mod
+
+- recommend
+-- call recommendation index (user, module)
+-- call total likes (module)
+-- call liked users (module)
+-- call jaccard index (user)
+*/
 
 //calculate jaccard index
-function jaccardIndex(user1, user2) {
-    console.log("1. calculating jaccard");
-    numerator = _.size(_.intersectionBy(user1, user2, 'likedMod'));
-    denominator = _.size(_.unionBy(user1, user2, 'likedMod'));
-    return parseFloat(numerator) / parseFloat(denominator);
-}
-
-//filter users who liked a mod
-function likedUsers(module) {
-    console.log("2. filtering users");
+// map to generate jaccard?
+function jaccardIndex(user, users) {
     return new Promise(function(resolve, reject) {
-        users = [];
-        Event.aggregate([
-            { $match: { type : "likedMod"}},
-            { $match: { modId : module}},
-            { $group: { _id: '$userId'}}
-        ]).then(events => {
-            for (var i = 0; i < events.length; i++) {
-                users.push(events[i]);
-            }
-        })
-        resolve(users);
-    });
-}
-
-//calculate recommendation index
-function recommendationIndex(user, module) {
-    console.log("3. calculating index");
-    return new Promise((resolve, reject) => {
-        var users = likedUsers(module);
-        var sum = 0;
-        for (var i = 0; i < users.length; i++) {
-            sum += jaccardIndex(user, users[i]);
-        }
-        resolve(parseFloat(sum) / parseFloat(_.size(users)));
-    });
-}
-
-//TODO: find modules that user has not liked
-
-//save recommendation on Recommendation
-function recommend(user) {
-    console.log("4. calculating recommendation");
-    return new Promise((resolve, reject) => {
-        Mod.find().then(modules => {
-            var promises = [];
-            recommendations = [];
-            for (var i = 0; i < modules.length; i++) {
-                promises.push(recommendationIndex(user, modules[i]));
-            }
-            Promise.all(promises).then(function(res) {
-                for (var i = 0; i < modules.length; i++) {
-                    var mod = {
-                        modId: modules[i].modId,
-                        recIndex: res[i]
-                    };
-                    recommendations.push(mod);
-                }
-                recommendations = sortRecommendations(recommendations);
-                resolve(recommendations);
+        Event.aggregate([ 
+            { $group: { _id : "$userId", modId: {$push: "$modId"}}},
+            { $match: {_id : user}}
+        ]).then(function(res) {
+            //console.log("res is: ", res[0].modId);
+            Event.aggregate([
+                { $group: { _id : "$userId", mods: {$push: "$modId"}}},
+                { $project: {
+                    "intersect": {"$size": { "$setIntersection": [res[0].modId, "$mods"]}}, 
+                    "union": {"$size": { "$setUnion": [res[0].modId, "$mods"] }},
+                    "jaccard": 
+                        { "$divide": [{"$size": { "$setIntersection": [res[0].modId, "$mods"]}},
+                              {"$size": { "$setUnion": [res[0].modId, "$mods"]}}]},
+                }},
+                { $group: { _id : 0, total: {$sum: "$jaccard"}}},
+            ]).then(function (res2) {
+                console.log(res2);
+                resolve(res2[0].total);
             });
         });
     });
 }
 
-//sort recommendation by recommendation index
-function sortRecommendations(recommendations) {
-    console.log("5. sorting recommmendations");
-    for (var i = 1; i < recommendations.length; i++) {
-        for (var j = 0; j < recommendations.length - 1; j++) {
-            if (recommendations[j].recIndex < recommendations[j+1].recIndex) {
-                var temp = recommendations[j];
-                recommendations[j] = recommendations[j+1];
-                recommendations[j+1] = temp;
-            }
-        }
-    }
-    return recommendations;
-}
-
-/*
-//generate recommendations for all users
-function generateRecommendations() {
-    console.log("6. generating recommmendations");
-    User.find().then(users => {
-        var sortedRecommendations = [];
-        for (var i = 0; i < users.length; i++) {
-            var recommendation = sortRecommendations(recommend(users[i]._id));
-            for (var j = 0; j < recommendation.length; j++) {
-                sortedRecommendations.push(recommendation[j].modId);
-            }
-            const newRecommendation = new Recommendation({
-                userId: users[i]._id,
-                recommendations: sortedRecommendations, //store string array of mods
-                type: recommendationTypesEnum.likes
-            });
-            newRecommendation.save()
-        }
+//count total number of likes for mod
+function totalLikes(module) {
+    return new Promise(function(resolve, reject) {
+        Event.aggregate([
+            {$match: {type: "likedMod"}},
+            {$match: {modId: module}},
+            {$count: "totalLikes"}
+        ]).then(function (res) {resolve(res[0].totalLikes)});
     });
 }
 
-generateRecommendations();
-console.log("Successfully generated recommendations");*/
+//filter users who liked a mod
+function likedUsers(module) {
+    return new Promise(function(resolve, reject) {
+        Event.aggregate([
+            { $match: { type : "likedMod"}},
+            { $match: { modId : module}},
+            { $group: { _id: '$userId'}}
+        ]).then(function (res) {resolve(res)});
+    });
+    
+}
+
+//calculate recommendation index
+function recommendationIndex(user, module) {
+    console.log("user is: ", user);
+    return new Promise((resolve, reject) => {
+        var users = likedUsers(module);
+        users.then(function(u) {
+            console.log("liked users: ", u);
+            var index = jaccardIndex(user, u);
+            index.then(function(ind) {
+                var total = totalLikes(module);
+                total.then(function(likes) {
+                    console.log("total likes is: ", likes);
+                    console.log("rec index is: ", ind / likes);
+                    resolve(ind / likes);
+                });
+            });
+        });
+        
+    })
+}
+
+function recommend(user) {
+    return new Promise((resolve, reject) => {
+        Mod.aggregate([
+            { $project: {
+                "count": {"$sum" : "$_id"},
+                "index": recIdx(user, "$_id")}
+                }
+        ]).then(function (res) {console.log(res); resolve(res)});
+    });
+}
+
+/*
+//save recommendation on Recommendation
+function recommend(user) {
+    Mod.find().then(modules => {
+        var promises = [];
+        recommendations = [];
+        for (var i = 0; i < modules.length; i++) {
+            promises.push(recommendationIndex(user, modules[i]));
+        }
+        Promise.all(promises).then(function(res) {
+            for (var i = 0; i < modules.length; i++) {
+                var mod = {
+                    modId: modules[i].modId,
+                    recIndex: res[i]
+                };
+                recommendations.push(mod);
+            }
+            const newRecommendation = new Recommendation({
+                userId: user._id,
+                recommendations: recommendations, //store string array of mods
+                type: recommendationTypesEnum.likes
+            });
+            newRecommendation.save();
+            resolve(recommendations);
+        });
+    });
+}*/
+
+console.log("generate");
+console.log(recommendationIndex(ObjectId("5f854114eee454084b1f9eab"), ObjectId("5f854114eee454084b1f9fb0")));
+//console.log(recommend(ObjectId("5f854114eee454084b1f9eab")));
