aa2fdf9ef72dbb8dbcf8b039c0deb117da817e2e
rongrongrr <jingrong.contact@gmail.com>
2020-10-30 10:18:19 +0800

    Fix bugs

--- api/src/utils/recommend/recommendByLikes.js
+++ api/src/utils/recommend/recommendByLikes.js
@@ -29,7 +29,7 @@ function jaccardIndex(user, users) {
             { $group: {_id : "$userId", subId: {$push: "$subId"}}},
             { $match: {_id : user}}
         ]).then(function(res) {
-            //console.log("res is: ", res[0].modId);
+            console.log("res is: ", res);
             Event.aggregate([
                 { $match: {sub : "mod"}},
                 { $match: {userId : {"$in": users}}},
@@ -54,7 +54,7 @@ function jaccardIndex(user, users) {
 function totalLikes(module) {
     return new Promise(function(resolve, reject) {
         Event.aggregate([
-            {$match: {type: "like"}},
+            {$match: {type: "view"}},
             {$match: {subId: module}},
             {$count: "totalLikes"}
         ]).then(function (res) {
@@ -66,7 +66,7 @@ function totalLikes(module) {
 function likedUsers(module) {
     return new Promise(function(resolve, reject) {
         Event.aggregate([
-            { $match: { type : "like"}},
+            { $match: { type : "view"}},
             { $match: { subId : module}},
             { $group: { _id: '$userId'}}
         ]).then(function (res) {
@@ -83,15 +83,12 @@ function likedUsers(module) {
 function recommendationIndex(user, module) {
     return new Promise((resolve, reject) => {
         var users = likedUsers(module);
-        users.then(function(u) {
+        return users.then(function(u) {
             var index = jaccardIndex(user, u);
-            index.then(function(ind) {
-                var total = totalLikes(module);
-                total.then(function(likes) {
-                    console.log("total likes is: ", likes);
-                    console.log("rec index is: ", ind / likes);
-                    resolve(ind / likes);
-                });
+            return index.then(function(ind) {
+                console.log("total likes is: ", u.length);
+                console.log("rec index is: ", ind / u.length);
+                resolve(ind / u.length);
             });
         });
         
@@ -104,25 +101,31 @@ function recommend(user) {
             if(err) {
                 console.log(err);
             } 
-            var recommendations;
+            var recs = {};
             modules.map(module => {
                 var index = recommendationIndex(user, module._id);
+                console.log("idx", index);
                 index.then(function(idx) {
-                    recommendations.module = idx;
-                })
+                    console.log("code, index ", code, idx);
+                    recs.module.code = idx;
+                    console.log(recs);
+                });
             });
+
+            /*
             
             const newRecommendation = new Recommendation({
                 userId: user._id,
-                recommendations: recommendations, //store key value pair of mod code to rec idx
+                recommendations: recs, //store key value pair of mod code to rec idx
                 type: recommendationTypesEnum.likes
             });
-            resolve(newRecommendation.save());
+            resolve(newRecommendation.save());*/
+            resolve(recs);
         });
     });
 }
 
-likedUsers(ObjectId("5f9b1c53c497871dbf23e702"));
-totalLikes(ObjectId("5f9b1c53c497871dbf23e702"));
-recommendationIndex(ObjectId("5f9b1c53c497871dbf23e65e"), ObjectId("5f9b1c53c497871dbf23e702"));
-recommend(ObjectId("5f9b1c53c497871dbf23e65e"));
+//likedUsers(ObjectId("5f9b1c53c497871dbf23e702"));
+//totalLikes(ObjectId("5f9b1c53c497871dbf23e702"));
+//recommendationIndex(ObjectId("5f9b64e79aa8ac9046e1278b"), ObjectId("5f9b64ec9aa8ac9046e13389"));
+recommend(ObjectId("5f9b64e79aa8ac9046e1278b"));
