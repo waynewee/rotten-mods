64743cc7d89463230ac0b6e69acda677d5d0b9df
Chester Sim <chestersimdy@gmail.com>
2020-11-01 01:48:19 +0800

    Update login button UI

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,26 +1,10 @@
-import { useEffect } from "react";
 import { NextPage } from "next";
 
 import HomeModuleList from "../components/HomeModuleList";
 import SectionTitle from "../components/SectionTitle";
 import ModuleCompareModal from "../components/ModuleCompareModal";
-import axios from "axios";
 
-const Home: NextPage = ({ }) => {
-
-  // Mock Login API call
-  // useEffect(() => {
-  //   axios.post("http://localhost:8080/api/login", {
-  //     email: "xnZZLvmrjsqrNFJd@email.edu",
-  //     password: "12345"
-  //   }, {
-  //     withCredentials: true
-  //   }).then(res => {
-  //     console.log("res:", res)
-  //   })
-  //     .catch(err => console.log("err:", err))
-  // }, []);
-
+const Home: NextPage = ({}) => {
   return (
     <>
       <ModuleCompareModal />
