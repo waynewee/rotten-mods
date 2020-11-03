5b461f76721ef399cc95b4379e674258237b4084
rongrongrr <jingrong.contact@gmail.com>
2020-11-04 04:04:27 +0800

    Add recommendations based on average rating, total rating and view count

---
+++ api/src/utils/recommend/recommendMostRated.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const { mongoUri } = require('../../../dist/config')
+
+mongoose.connect(mongoUri)
+
+const Rating = require('../../../dist/src/models/Rating').default
+const Mod = require('../../../dist/src/models/mod').default
+var _ = require('lodash');
+
+function recommend() {
+  return Rating.aggregate([
+    { $match: {sub : "mod"}},
+    { $match: {type : "star"}},
+    { $group: {_id : "$subId", ratings: {$sum: 1}}},
+    { $sort: {ratings : -1}},
+    {$limit: 10}
+  ]).then(res => {
+    //console.log(res)
+    res = res.map(r => r._id)
+    //console.log(res)
+    return Mod.aggregate([
+      { $project: {_id: {$toString: "$_id"}, code: 1}},
+      { $match: {_id : {"$in": res}}},
+    ]).then(res2 => {
+      res2 = res2.map(r => r.code)
+      console.log(res2)
+      return res2
+    })
+  })
+}
+
+/*
+To generate all time 10* most rated modules as recommendations:
+- call recommend()
+- returns array of mods sorted in descending order of rating count
+*/
+recommend()
+
