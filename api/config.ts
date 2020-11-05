e01bf5788feb1cf1e2c2c32519ffb703d410bd92
waynewee <wwayne.wee@gmail.com>
2020-11-05 13:28:52 +0800

    updated config

--- api/config.ts
+++ api/config.ts
@@ -1,11 +1,9 @@
-import e from "express";
-
 const production = process.env.NODE_ENV === 'production'
 
 let mongoUri
 
 if( production ){
-  mongoUri = 'mongodb://18.141.176.139/rotten-mods';
+  mongoUri = 'mongodb://127.0.0.1:27017/rotten-mods';
 } else {
   mongoUri = 'mongodb://127.0.0.1:27017/rotten-mods';
 }
