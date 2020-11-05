8830e6068d15bf3aa7923986ecdd7e5b59abaa87
waynewee <wwayne.wee@gmail.com>
2020-11-05 12:29:31 +0800

    changed config of mongo

--- api/config.ts
+++ api/config.ts
@@ -1,1 +1,15 @@
-export const mongoUri:string = 'mongodb://127.0.0.1:27017/rotten-mods';
+import e from "express";
+
+const production = process.env.NODE_ENV === 'production'
+
+let mongoUri
+
+if( production ){
+  mongoUri = 'mongodb://18.141.176.139/rotten-mods';
+} else {
+  mongoUri = 'mongodb://127.0.0.1:27017/rotten-mods';
+}
+
+export {
+  mongoUri
+}
